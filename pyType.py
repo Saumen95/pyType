594d2ce3f90f3f848bf61a4604968e46d44d29d4
Saumen95 <saumenroy323@gmail.com>
2020-07-07 11:29:39 +0600

    Save data to pastebin

--- pyType.py
+++ pyType.py
@@ -19,3 +19,9 @@ class Game(object):
         pygame.init()
         self.open_img = pygame.image.load('type-speed-open.png')
         self.open_img = pygame.transform.scale(self.open_img, (self.w, self.h))
+
+    def draw_txt(self, screen, msg, y, fsize, color):
+        font = pygame.font.Font(None, fsize)
+        text = font.render(msg, 1, color)
+        text_rect = text.get_rect(centre=(self.w/2, y)
+        screen.blit(text, text_rect)
